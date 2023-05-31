37580deb0782d867609656dc65cfb86fa00bc2bd
Kennedy-Torres <krtorres.programador@hotmail.com>
2023-05-31 01:09:50 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,24 +5,61 @@ typedef struct{
     float peso, valor_compra, valor_venda;
     int id_uf, id_fabricante;
     int id_produto;
-}Produto;
+}PRODUTO;
 
 typedef struct{
     char nome_marca[30], site[50];
     int telefone, id_uf;
     int id_fabricante;
-}Fabricante;
+}FABRICANTE;
 
 typedef struct{
     char nome[30];
     int id_uf;
-}Uf;
+}UF;
 
 int main(){
 
-    Produto produto[50];
-    Fabricante fabricante[5];
-    Uf uf[27];
+    PRODUTO produto[50];
+    FABRICANTE fabricante[5];
+    UF uf[27];
+
 
     return 0;
 }
+
+/*--------------------------- [1]LISTAR TODAS AS MARCAS -----------------*/
+void RegistrarMarca(FABRICANTE* fabricante){
+    int i=0;
+    char confirmacao;
+
+    while(i<2){
+        printf("Informe o nome da %da marca: ",i+1); //minimo de 2 marcas registradas
+        scanf(" %[^\n]s", &fabricante[i].nome_marca);
+        i++;
+    }
+
+    do{
+        printf("Deseja cadastrar outra marca:(S/N)\n>");
+        scanf(" %c", &confirmacao);
+        
+        if( (confirmacao == 's' || confirmacao == 'S') && i<5){
+            printf("Informe o nome da %da marca: ",i+1);
+            scanf(" %[^\n]s", &fabricante[i].nome_marca);
+        }
+        i++;
+    }while( (confirmacao == 's' || confirmacao == 'S') && i<5 );
+}
+
+void RegistrarSite(){
+
+}
+
+void RegistrarTelefone(){
+
+}
+
+void RegistrarUf(){
+
+}
+/*-----------------------------------------------------------------------*/
