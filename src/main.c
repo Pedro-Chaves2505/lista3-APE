da2d081dea44d33911ed367b30379969c22e6b0a
Kennedy-Torres <krtorres.programador@hotmail.com>
2023-05-30 23:51:39 -0300

    Implantação das structs
    Inserido struct's e variáveis: Produto, Fabricante, Uf

--- src/main.c
+++ src/main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int main()
-{
+typedef struct{
+    char descricao[50];
+    float peso, valor_compra, valor_venda;
+    int id_uf, id_fabricante;
+    int id_produto;
+}Produto;
+
+typedef struct{
+    char nome_marca[30], site[50];
+    int telefone, id_uf;
+    int id_fabricante;
+}Fabricante;
+
+typedef struct{
+    char nome[30];
+    int id_uf;
+}Uf;
+
+int main(){
+
+    Produto produto[50];
+    Fabricante fabricante[5];
+    Uf uf[27];
 
     return 0;
 }
